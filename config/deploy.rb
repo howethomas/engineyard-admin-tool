89218eba136dd641992b623d0a685ce305f13950
Jay Phillips <jicksta+git@gmail.com>
2008-02-13 16:05:48 -0800

    Wrote a Capfile for the Rails deployment that seems to work well so far

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,21 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "pbx-gui"
+set :user, "jicksta" # Must change to 'deploy'!
+set :use_sudo, false
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+# Git/Github setup
+set :scm, :git
+set :repository,  "git@github.com:jicksta/engineyard-admin-tool.git"
+
+set :project_deploy_to_root, "/usr/local/engineyard"
+
+set :deploy_to, "#{project_deploy_to_root}/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+set :main_server, "10.0.1.195"
+
+role :app, main_server
+role :web, main_server
+# role :db,  "your db-server here", :primary => true
